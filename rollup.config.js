010c520a25fe64e36c5e22d6191088a102d99081
ytahirkose <yasar.kose@elsitel.com.tr>
2024-09-06 14:47:56 +0300

    file name changed

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default [
     {
         input: 'src/index.js',
         output: {
-            file: 'lib/index.js',
+            file: 'dist/index.js',
             format: 'es',
             sourcemap: devMode ? 'inline' : false,
             plugins: [
